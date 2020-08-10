0eea58cfe2f3319a11b887f94b95643c4dce62d0
Leo Velez <leonardo.velez@kaplan.edu>
2020-08-10 12:25:09 -0400

    Character Sheet initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterBuilderComponent } from './character-builder/character-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterBuilderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/character-builder/character-builder.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-builder',
+  templateUrl: './character-builder.component.html',
+  styleUrls: ['./character-builder.component.scss']
+})
+export class CharacterBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  growWidth(event: any) {
+    event.target.style.width = '0px';
+    event.target.style.width = (event.target.scrollWidth + 5) + 'px';
+  }
+
+}
